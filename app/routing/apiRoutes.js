a01ee0fdbb3f73d86387a17de705eddf0bc6e312
Meridith Berk <mberk8855@gmail.com>
2019-07-22 22:46:17 -0700

    update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on all possible friends
+// ===============================================================================
+
+var friends = require("../data/friends");
+console.log(friends)
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  console.log("api routes")
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+      console.log("I am in the post route, pending for matching")
+      console.log(req.body)
+
+      // friends array inside the objects is an array with the score
+
+      // loop and compare the each score with the score in the body (req.body.scores)
+
+      // figure out who is the best match
+
+      // res.json(result)
+   //your code here
+    
+  });
+};
